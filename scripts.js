72accd36a5faefd40fb9bbf766fa4583267e0a85
Luther <luther.m.cordeiro@gmail.com>
2020-07-27 02:27:01 -0300

    Scripts, Modal & Links

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards){
+  card.addEventListener("click", function(){
+    const videoLink = card.getAttribute("src")
+    modalOverlay.classList.add('active')
+    modalOverlay.querySelector('iframe').src=`${videoLink}`
+  })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+  modalOverlay.classList.remove("active")
+  modalOverlay.querySelector('iframe').src=``
+})
